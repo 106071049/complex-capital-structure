8eeb5f038a57096888334c8ba320f3f70a38994c
Louis <gooshibaga@gmail.com>
2026-02-15 14:54:14 +0800

    Add basePath for reverse proxy integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/complex-capital',
+  assetPrefix: '/complex-capital',
   typescript: {
     ignoreBuildErrors: true,
   },
